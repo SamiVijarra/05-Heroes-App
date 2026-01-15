b6668eeab2d7e7f01602f4cd22900d0e450c7f2c
SamiVijarra <samivijarra1@gmail.com>
2026-01-15 11:29:02 -0300

    breadcrumb

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,6 +1,7 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
 import { SearchControls } from "./ui/SearchControls";
+import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
 
 export const SearchPage = () => {
   return (
@@ -9,6 +10,12 @@ export const SearchPage = () => {
                 title="Búsqueda de SuperHéroes"
                 description="Descubre, explora y administra superhéroes y villanos"
       />
+      <CustomBreadcrumbs currentPage="Buscador de Héroes"
+        breadcrumbs={[
+          { label: 'Home1', to: '/' },
+          { label: 'Home2', to: '/'},
+          { label: 'Home3', to: '/'},
+      ]}/>
       <HeroStats />
       <SearchControls/>
     </>
